4c4db0a7b121109f778b9243def4710fe0cc7072
Marios <mariosant@sent.com>
2021-06-24 19:59:54 +0300

    Amend delete modal design

--- src/pages/settings/DeleteModal.jsx
+++ src/pages/settings/DeleteModal.jsx
@@ -3,7 +3,7 @@ import { TrashIcon } from "@heroicons/react/solid";
 
 const ConfirmButton = ({ children, ...props }) => (
   <button
-    className="px-3 py-2 font-semibold text-white rounded whitespace-nowrap bg-red500 hover:bg-red600"
+    className="w-32 px-3 py-2 mx-1 font-semibold text-white rounded whitespace-nowrap bg-red500 hover:bg-red600"
     {...props}
   >
     {children}
@@ -11,7 +11,10 @@ const ConfirmButton = ({ children, ...props }) => (
 );
 
 const CancelButton = ({ children, ...props }) => (
-  <button className="text-subtle whitespace-nowrap hover:text-body" {...props}>
+  <button
+    className="w-32 px-3 py-2 mx-1 font-semibold bg-white border rounded hover:bg-gray50 border-gray150 text-body whitespace-nowrap"
+    {...props}
+  >
     {children}
   </button>
 );
@@ -24,21 +27,22 @@ const DeleteModal = ({ onClose, onConfirm, pointName, ...props }) => {
       {...props}
     >
       <div className="flex items-center justify-center min-h-screen">
-        <div className="z-20 flex flex-col items-center max-w-sm p-4 mx-auto text-center bg-white rounded-large">
+        <div className="z-20 flex flex-col items-center max-w-lg p-10 mx-auto text-center bg-white rounded">
           <TrashIcon className="w-16 h-16 mb-6 text-heading" />
 
-          <Dialog.Title className="mb-4 text-xl font-semibold text-heading">
+          <Dialog.Title className="mb-4 text-2xl font-semibold text-heading">
             Are you sure to delete this place?
           </Dialog.Title>
 
           <div className="w-full mb-4 overflow-hidden whitespace-nowrap overflow-ellipsis">
             {pointName}
           </div>
-          <p className="mb-4 text-body">
+
+          <p className="mb-8 text-body">
             Deleting places is <strong>an irreversible action</strong>.
           </p>
 
-          <div className="flex justify-around w-1/2 m-auto">
+          <div className="flex justify-center w-full">
             <CancelButton onClick={onClose}>Cancel</CancelButton>
             <ConfirmButton onClick={onConfirm}>Delete place</ConfirmButton>
           </div>
